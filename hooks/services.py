70a283d660b522978d34ea9634ecee3e27c82af7
Wayne Witzel III <wayne@riotousliving.com>
2015-04-13 11:20:39 -0400

    added repo cloning support

--- hooks/services.py
+++ hooks/services.py
@@ -2,9 +2,14 @@
 
 from charmhelpers.core.services.base import Manager
 from charmhelpers.core.services import helpers
+from charmhelpers.fetch import install_remote
+from charmhelpers.core import hookenv
 
 import actions
 
+
+def clone_repo():
+    install_remote(hookenv.config('repo'))
 
 def manage():
     manager = Manager([
@@ -19,12 +24,14 @@ def manage():
                 # data (contexts) required to start the service
                 # e.g.: helpers.RequiredConfig('domain', 'auth_key'),
                 #       helpers.MysqlRelation(),
+                helpers.config_is_set('repo'),
                 helpers.MySQLRelation(optional=True),
             ],
             'callbacks': [
+                clone_repo,
                 helpers.render_template(
-                    source='upstart.conf',
-                    target='/etc/init/juju-pyramid'),
+                    source='/home/ubuntu/repo/juju.ini.tmpl',
+                    target='/home/ubuntu/repo/juju.ini'),
                 actions.log_start,
             ],
         },
